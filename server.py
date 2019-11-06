0e8eac3378ee9262e41633859da73690030e3b3d
miaoshihu <miaoshihu@gmail.com>
2019-11-06 20:07:29 +0800

    update wechat file

--- server.py
+++ server.py
@@ -5,8 +5,9 @@ import os
 from application import Application
 from config import options
 from tornado.httpserver import HTTPServer
-from index import WechatLoginHandler, IndexHandler, UploadHandler, GoodGetListHandler
+from index import IndexHandler, UploadHandler, GoodGetListHandler
 from api.good import GoodSubmit
+from api.wechat import WechatLoginHandler
 
 app = tornado.web.Application([
     (r'/fps/wechat_login', WechatLoginHandler),
